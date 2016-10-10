3b15b8d9807e2d43967136b153c046de37c35b4e
Tux <tux2mc@gmail.com>
2016-10-10 09:14:51 -0400

    Gonna rework the Freeze Ice + Vanish Feather

--- src/main/java/net/tux22193/staffmode/StaffMode.java
+++ src/main/java/net/tux22193/staffmode/StaffMode.java
@@ -3,7 +3,6 @@ package net.tux22193.staffmode;
 import net.tux22193.staffmode.commands.StaffCommand;
 import net.tux22193.staffmode.listeners.RandomTeleport;
 import net.tux22193.staffmode.listeners.RodAlerts;
-import net.tux22193.staffmode.listeners.VanishFeather;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,6 +14,5 @@ public class StaffMode extends JavaPlugin {
         StaffCommand.enabled.clear();
         Bukkit.getServer().getPluginManager().registerEvents(new RandomTeleport(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new RodAlerts(), this);
-        Bukkit.getServer().getPluginManager().registerEvents(new VanishFeather(), this);
     }
 }
