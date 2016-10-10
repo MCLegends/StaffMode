f31119d5de04361c9096322e4e60f7b025ef0657
Tux22193 <tux2mc@gmail.com>
2016-10-09 20:48:03 -0400

    Register events

--- src/main/java/net/tux22193/staffmode/StaffMode.java
+++ src/main/java/net/tux22193/staffmode/StaffMode.java
@@ -1,6 +1,11 @@
 package net.tux22193.staffmode;
 
 import net.tux22193.staffmode.commands.StaffCommand;
+import net.tux22193.staffmode.listeners.RandomTeleport;
+import net.tux22193.staffmode.listeners.RodAlerts;
+import net.tux22193.staffmode.listeners.VanishFeather;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.List;
 
@@ -14,5 +19,8 @@ public class StaffMode extends JavaPlugin {
     @Override
     public void onEnable() {
         this.getCommand("staff").setExecutor(new StaffCommand());
+        Bukkit.getServer().getPluginManager().registerEvents(new RandomTeleport(), this);
+        Bukkit.getServer().getPluginManager().registerEvents(new RodAlerts(), this);
+        Bukkit.getServer().getPluginManager().registerEvents(new VanishFeather(), this);
     }
 }
